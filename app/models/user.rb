054c1a12c7aaf6f47cfc737414044becb6248128
Matt <sosei@internode.on.net>
2018-05-11 02:42:52 +1000

    styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ApplicationRecord
+  include Authority::UserAbilities
   rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  include Authority::UserAbilities
+  
   has_many :products, foreign_key: :author_id
+  def has_role?(*role_names)
+    self.roles.where(:name => role_names).present?
+  end
 end
