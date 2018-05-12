572f616194a8413fc62c6e914ff476f7c61660ff
Matt <sosei@internode.on.net>
2018-05-12 23:50:17 +1000

    dashboard permissions, product listing, navbar styling

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,17 @@
 class DashboardController < ApplicationController
+
+before_action :authenticate_user!
+before_action :check_user
+
   def index
+    @user = current_user
+    @products = Product.where(user: current_user)
+  end
+
+  def check_user
+    unless current_user.has_role?(:bcmember)
+      redirect_to root_url, alert: "Sorry, you do not have access to this content."
+    end
   end
 end
+
