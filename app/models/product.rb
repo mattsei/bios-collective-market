e1e960bd3db75c6185d3c5cb3c8372350b03e982
Matt <sosei@internode.on.net>
2018-05-20 22:46:49 +1000

    DONE

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,4 +7,11 @@ class Product < ApplicationRecord
 
   validates :name, :description, :price, presence: true
   validates :price, numericality: true, format: { with: /\A\$?[0-9]+\.?[0-9]?[0-9]?\z/, message: "Must be a dollar amount"}
+
+
+
+  scope(:product_name, -> (name) { where("name like ?", "#{name}%")})
+  # scope(:owner, -> (product_user) { where owner: @product_user})
+  scope(:price, -> (price) { where price: price })
+
 end
